41517d7834e1affd5e7bf1fdb167c3494487b440
Deepansh <deepanshsrivastav9711@gmail.com>
2023-08-05 20:28:35 +0530

    Changed the button from hamburger to cross when the navbar - menu is open

--- NavBar.js
+++ NavBar.js
@@ -3,6 +3,18 @@ let button = document.querySelector('.hamburger')
 let navLinks = document.querySelector('.nav-links')
 
 
+let toggleButton = ()=>{
+    let currentButton = button.src
+    let newButton = 'http://127.0.0.1:5500/Navbar/ICON/Cross.png'
+
+    return (currentButton===newButton? button.src = 'http://127.0.0.1:5500/Navbar/ICON/Hamburger.png': button.src = newButton  )
+}
+
+
 button.addEventListener('click',()=>{
+
+    toggleButton()
+
     navLinks.classList.toggle('active')
 })
+
